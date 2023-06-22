b9247e52f3620e8794665ce03d9cb41559d1e309
Toxic <markusbolsen@gmail.com>
2023-06-22 18:11:31 +0200

    Added simple Vector projection functions.

--- Utility/Util.cs
+++ Utility/Util.cs
@@ -1,4 +1,5 @@
 ﻿
+using OpenTK.Mathematics;
 using System.Runtime.CompilerServices;
 
 namespace OpenTKEngine.Utility;
@@ -8,4 +9,12 @@ public static class Util
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float Rlerp(float inter, float inStart, float inEnd, float outStart, float outEnd)
         => (outEnd - outStart) / (inEnd - inStart) * (inter - inEnd) + outEnd;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Proj(this Vector2 vec1, Vector2 vec2)
+        => Vector2.Dot(vec1, vec2) / vec2.LengthSquared * vec2;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float ProjLength(this Vector2 vec1, Vector2 vec2)
+        => Vector2.Dot(vec1, vec2) / vec2.LengthSquared * vec2.Length;
 }
