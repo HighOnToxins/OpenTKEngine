080f92f93b0ff05b196b427b3465b01c52af1c18
Toxic <markusbolsen@gmail.com>
2023-06-26 09:06:39 +0200

    Added mesh.

---
+++ Rendering/Mesh.cs
@@ -0,0 +1,109 @@
+﻿
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTKEngine.Rendering.GLObjects;
+
+namespace OpenTKEngine.Rendering;
+
+public abstract class Mesh: IDisposable
+{
+    public static readonly Mesh<Vector3> Box = new(
+        new Vector3[]
+        {
+            new( 1,  1, 0),
+            new( 1, -1, 0),
+            new(-1,  1, 0),
+            new(-1, -1, 0),
+        },
+        new uint[]
+        {
+            0, 1, 2, 1, 2, 3
+        }
+    );
+
+    private readonly VertexBuffer<uint>? elementBuffer;
+
+    public bool HasElements { get => elementBuffer is not null; }
+
+    public Mesh(params uint[] elements) 
+    { 
+        if(elements.Length != 0)
+        {
+            elementBuffer = new VertexBuffer<uint>(BufferTargetARB.ElementArrayBuffer, elements);
+        }
+    }
+
+    public virtual int SetBuffers(VertexArray array, params ProgramAttribute[] attributes)
+    {
+        if(elementBuffer is not null)
+        {
+            array.SetElementBuffer(elementBuffer);
+        }
+
+        return 0;
+    }
+
+    public virtual void Dispose()
+    {
+        elementBuffer?.Dispose();
+    }
+
+}
+
+
+public class Mesh<T> : Mesh where T : unmanaged
+{
+
+    private readonly VertexBuffer<T> buffer;
+
+    public Mesh(T[] vertices, params uint[] elements) : base(elements)
+    {
+        buffer = new(vertices);
+    }
+
+    public Mesh(params T[] vertices) : this(vertices, Array.Empty<uint>())
+    {
+    }
+
+    public override int SetBuffers(VertexArray array, params ProgramAttribute[] attributes)
+    {
+        base.SetBuffers(array, attributes);
+        array.SetBuffer(buffer, 0, attributes[..buffer.AttributeTypes.Length]);
+        return buffer.AttributeTypes.Length;
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        buffer.Dispose();
+    }
+}
+
+public class Mesh<T1, T2>: Mesh<T1> where T1: unmanaged where T2: unmanaged
+{
+    private readonly VertexBuffer<T2> buffer;
+
+    public Mesh((T1, T2)[] vertices, params uint[] elements) : 
+        base(vertices.Select(v => v.Item1).ToArray(), elements)
+    {
+        buffer = new(vertices.Select(v => v.Item2).ToArray());
+    }
+
+    public Mesh((T1, T2)[] vertices) : this(vertices, Array.Empty<uint>())
+    {
+    }
+
+    public override int SetBuffers(VertexArray array, params ProgramAttribute[] attributes)
+    {
+        int offset = base.SetBuffers(array, attributes);
+        array.SetBuffer(buffer, 0, 
+            attributes[(offset+1)..(offset+1+buffer.AttributeTypes.Length)]);
+        return offset + buffer.AttributeTypes.Length;
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        buffer.Dispose();
+    }
+}
