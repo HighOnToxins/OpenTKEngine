57f0eef1bec9d3cf5883ef068e1d560f77de8faa
Toxic <markusbolsen@gmail.com>
2023-07-01 14:51:23 +0200

    Changed from Shape to ShapeInstance

--- Utility/ShapeInstance.cs
+++ Utility/ShapeInstance.cs
@@ -3,27 +3,27 @@ using OpenTK.Mathematics;
 
 namespace OpenTKEngine.Utility;
 
-public struct Shape
+public struct ShapeInstance
 {
-    public Shape(Vector3 position, Vector3 size, Quaternion quaternion, Vector4 color)
+    public ShapeInstance(Vector3 position, Vector3 size, Quaternion quaternion, Vector4 color)
     {
         model = Matrix4.CreateScale(size) * Matrix4.CreateFromQuaternion(quaternion) * Matrix4.CreateTranslation(position);
         this.color = color;
     }
 
-    public Shape(Vector3 position, Vector3 size, Vector3 normal, float rotation, Vector4 color) :
+    public ShapeInstance(Vector3 position, Vector3 size, Vector3 normal, float rotation, Vector4 color) :
         this(position, size, Quaternion.FromAxisAngle(normal, rotation), color)
     { }
 
-    public Shape(Vector3 position, Vector3 size, Vector4 color) :
+    public ShapeInstance(Vector3 position, Vector3 size, Vector4 color) :
         this(position, size, Quaternion.Identity, color)
     { }
 
-    public Shape(Vector2 position, Vector2 size, float rotation, Vector4 color) : 
+    public ShapeInstance(Vector2 position, Vector2 size, float rotation, Vector4 color) : 
         this(new Vector3(position, 0), new Vector3(size, 1), Vector3.UnitZ, rotation, color)
     { }
 
-    public Shape(Vector2 position, Vector2 size, Vector4 color) :
+    public ShapeInstance(Vector2 position, Vector2 size, Vector4 color) :
         this(new Vector3(position, 0), new Vector3(size, 1), color)
     { }
 
