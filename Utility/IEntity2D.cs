47bc7a6eb79c6656874e2eb3684d8979a4c1c921
Toxic <markusbolsen@gmail.com>
2023-07-01 15:05:14 +0200

    Added entity and physics entity interfaces.

---
+++ Utility/IEntity2D.cs
@@ -0,0 +1,17 @@
+﻿
+using OpenTK.Mathematics;
+
+namespace OpenTKEngine.Utility;
+
+public interface IEntity2D
+{
+    public Vector2 PositionOffset { get; }
+
+    public Vector2 Position { get; }
+
+    public Vector2 Size { get; }
+
+    public float Rotation { get; }
+}
+
+//TODO: Add shape-type.
---
+++ Utility/IPhysicsEntity2D.cs
@@ -0,0 +1,11 @@
+﻿
+using OpenTK.Mathematics;
+
+namespace OpenTKEngine.Utility;
+
+public interface IPhysicsEntity2D : IEntity2D
+{
+    public Vector2 Velocity { get; }
+
+    public float Mass { get; set; }
+}
